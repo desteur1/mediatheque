5994c63ba376788c41c8a70b8735933ef9d193bc
Desteur Fall <shmurdanoah@gmail.come>
2025-10-05 08:53:25 +0200

    create admin-member-test and medias

--- notre_livre/urls.py
+++ notre_livre/urls.py
@@ -6,4 +6,5 @@ urlpatterns =[
     path ('', views.liste_medias, name='liste_medias'),
     path ('<int:id>/', views.detail_medias, name='detail_medias'),
     path('<int:media_id>/emprunter/', views.emprunter_media, name='emprunter_media'),
+    path('<int:emprunt_id>/retourner/', views.retourner_media, name='retourner_media'),
 ]
